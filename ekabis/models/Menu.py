db6e66f908e44c8cbdd89753b5ad94b80c3d6121
brcdgn <doganbrc95@gmail.com>
2021-11-08 15:26:31 +0300

    menu ve permission data

--- ekabis/models/Menu.py
+++ ekabis/models/Menu.py
@@ -6,7 +6,7 @@ from ekabis.models.BaseModel import BaseModel
 class Menu(BaseModel):
     name = models.CharField(max_length=120, null=True)
     url = models.CharField(max_length=120, null=True, blank=True)
-    is_parent = models.BooleanField(default=True)
+    is_parent = models.BooleanField()
     is_show = models.BooleanField(default=True)
     fa_icon = models.CharField(max_length=120, null=True, blank=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
