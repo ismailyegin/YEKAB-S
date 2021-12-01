f084fb7a8daf199cff08a1515a874edb28a65607
brcdgn <doganbrc95@gmail.com>
2021-12-01 13:11:53 +0300

    date change

--- ekabis/Forms/YekaContractForm.py
+++ ekabis/Forms/YekaContractForm.py
@@ -7,17 +7,17 @@ from ekabis.models.YekaContract import YekaContract
 class YekaContractForm(BaseForm):
     class Meta:
         model = YekaContract
-        fields = ('price', 'unit', 'contract_date', 'company', 'contract',)
+        fields = ('price', 'unit', 'contractDate', 'company', 'contract',)
         labels = {
             'price': 'Fiyat',
             'contract': 'Kullanım Hakkı Sözleşmesi',
-            'contract_date': 'Sözleşme İmza Tarihi',
+            'contractDate': 'Sözleşme İmza Tarihi',
             'unit': 'Birimi',
             'company': 'Kazanan Firma', }
         widgets = {
             'unit': forms.Select(attrs={'class': 'form-control select2 select2-hidden-accessible',
                                         'style': 'width: 100%;'}),
-            'contract_date': forms.DateInput(
+            'contractDate': forms.DateInput(
                 attrs={'class': 'form-control  pull-right datepicker6', 'autocomplete': 'off',
                        'onkeydown': 'return true', "data-inputmask-alias": "datetime",
                        "data-inputmask-inputformat": "dd/mm/yyyy", "data-mask": "", "inputmode": "numeric"}),
