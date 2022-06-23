a6058cfba5d84dc03a69be9d129ef08edd0fe76e
brcdgn <doganbrc95@gmail.com>
2022-06-23 17:10:36 +0300

    cronjob test

--- ekabis/cron.py
+++ ekabis/cron.py
@@ -12,24 +12,24 @@ from ekabis.models import YekaCompetition, Notification, NotificationUser
 
 def my_scheduled_job():
     try:
-        print(datetime.now())
-        competitions = YekaCompetition.objects.filter(isDeleted=False).filter(
-            eskalasyon_first_date__isnull=False).filter(is_calculation=True)
-        date = datetime.now().date()
-        for competition in competitions:
-            compDate = competition.eskalasyon_first_date.split('-')
-            if str(date.month) and compDate[1] == str(date.year) :
-                EskalasyonCalculation(competition.uuid)
+        print(str(datetime.now())+'test eskalasyon hesabı ')
+        # competitions = YekaCompetition.objects.filter(isDeleted=False).filter(
+        #     eskalasyon_first_date__isnull=False).filter(is_calculation=True)
+        # date = datetime.now().date()
+        # for competition in competitions:
+        #     compDate = datetime.strptime(competition.eskalasyon_first_date, '%d-%m-%Y')
+        #     if compDate.month == date.month and compDate.year == date.year :
+        #         EskalasyonCalculation(competition.uuid)
     except:
         traceback.print_exc()
 
 
 def eskalasyon_date_job():
     try:
-        print('eskalasyon date ')
-        notification=Notification(not_description='TEST CRON NOTIFICATION',title=str(datetime.today())+'--CRONJOB')
-        notification.save()
-        NotificationUser(user=User.objects.filter(is_superuser=True).last(),notification=notification).save()
+        print(str(datetime.now()) + 'test eskalasyon tarih hesabı ')
+        # notification=Notification(not_description='TEST CRON NOTIFICATION',title=str(datetime.today())+'--CRONJOB')
+        # notification.save()
+        # NotificationUser(user=User.objects.filter(is_superuser=True).last(),notification=notification).save()
         # yekas = YekaCompetition.objects.filter(isDeleted=False).order_by('-date')
         # competitionEskalasyonDate(yekas)
     except:
--- scheduler/updater.py
+++ scheduler/updater.py
@@ -9,8 +9,8 @@ from ekabis.cron import my_scheduled_job, eskalasyon_date_job
 def start():
     scheduler = BackgroundScheduler(timezone='Europe/Istanbul')
     # cron1 = CronTrigger(day_of_week='mon-fri', hour='10', minute='34,35,36', timezone='Europe/Istanbul')
-    cron2 = CronTrigger(day_of_week='mon-fri', hour='00', minute='00', timezone='Europe/Istanbul')
-    cron3 = CronTrigger(hour='16', minute='50,55',month='6,7' ,timezone='Europe/Istanbul')
+    cron2 = CronTrigger(day_of_week='mon-fri', hour='00', minute='00',month='7' ,timezone='Europe/Istanbul') #ESKALASYON HESAPLANMASI
+    cron3 = CronTrigger(hour='16', minute='50,55',month='7' ,timezone='Europe/Istanbul') #ÖNCEKİ YARIŞMALARA AİT ESKALASYON TARİHLERİNİN GÜNCELLENMESİ
 
     trigger = OrTrigger([cron2])
     trigger3 = OrTrigger([cron3])
